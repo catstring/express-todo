bc23990eccfb2f7f20f4d922e97937dbf8e29755
Todd MC Tsai <toddmctsai@gmail.com>
2024-06-16 15:55:02 +0800

    move callbackURL to env

--- config/passport.js
+++ config/passport.js
@@ -7,7 +7,7 @@ const User = require('../models/user.model'); // Ensure you have this model file
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (token, tokenSecret, profile, done) => {
     try {
